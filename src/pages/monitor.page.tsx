c7a00bf4a4a3125ba79afa33110a4f45f8a7d049
Bobert <salvo.robertroy@gmail.com>
2025-05-31 00:09:03 +0800

    feat: added manual alert

--- src/pages/monitor.page.tsx
+++ src/pages/monitor.page.tsx
@@ -34,20 +34,34 @@ const MonitorPage = () => {
     // }, [setAlerts]);
 
     const [isLoaded, setIsLoaded] = useState(false);
+    const [isAlarm, setIsAlarm] = useState(false)
 
+    const handleTriggerAlarm = async () => {
+        setIsAlarm(!isAlarm)
+        
+        await fetch(`${process.env.NEXT_PUBLIC_ESP_URL!}${!isAlarm ? 'alert-on' : 'alert-off' }`)
+    }
 
     return (
-        <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-br from-gray-100 to-gray-300">
-            <div className="w-full max-w-[65%] bg-white rounded-2xl shadow-2xl p-4">
+        <div className="w-screen h-screen flex gap-10 items-center justify-center bg-gradient-to-br from-gray-100 to-gray-300">
+            <div className="w-full flex flex-col items-center gap-6  max-w-[55%] bg-white rounded-2xl shadow-2xl p-4">
             <h2 className="text-xl font-semibold text-gray-800 mb-4 text-center">
                 Live Camera Feed
             </h2>
+
             <img
                 src={process.env.NEXT_PUBLIC_CAM_URL}
                 onLoad={() => setIsLoaded(true)}
                 className='rounded-xl w-full shadow-md transition-opacity duration-500 opacity-100'
                 alt="Live feed"
             />
+              <button
+                    onClick={handleTriggerAlarm}
+                    className="bg-[#2C6E49] text-white px-6 py-2 font-bold rounded-full cursor-pointer hover:bg-[#3D8E64]"
+                    >
+                    Alarm: {!isAlarm ? 'OFF' : 'ON'}
+                </button>
+
             </div>
       </div>
     )
